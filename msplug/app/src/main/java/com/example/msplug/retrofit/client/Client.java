77c53d22d42e49051db2f0cb734e802270a66772
Adewole Opeyemi <58083371+adewoleopeyemi@users.noreply.github.com>
2021-01-23 12:13:01 -0800

    timeout increased to 60 secs

--- msplug/app/src/main/java/com/example/msplug/retrofit/client/Client.java
+++ msplug/app/src/main/java/com/example/msplug/retrofit/client/Client.java
@@ -1,16 +1,27 @@
 package com.example.msplug.retrofit.client;
 
+import java.util.concurrent.TimeUnit;
+
+import okhttp3.OkHttpClient;
 import retrofit2.Retrofit;
 import retrofit2.converter.gson.GsonConverterFactory;
 
 public class Client {
     private static Retrofit retrofit = null;
 
+
+
     public static Retrofit getRetrofit(String url){
         if (retrofit==null){
+            final OkHttpClient okHttpClient = new OkHttpClient.Builder()
+                    .readTimeout(60, TimeUnit.SECONDS)
+                    .connectTimeout(60, TimeUnit.SECONDS)
+                    .build();
+
             retrofit = new Retrofit.Builder()
                     .baseUrl(url)
                     .addConverterFactory(GsonConverterFactory.create())
+                    .client(okHttpClient)
                     .build();
         }
         return retrofit;
